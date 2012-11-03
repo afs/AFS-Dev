5472b7a350f8650f0a7da5192b7a687105484fa6
Andy Seaborne <andy.seaborne@gmail.com>
2012-11-03 22:15:10 +0000

    Sync

---
+++ src/main/java/projects/blkmgr/virtual/DiskBlock.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package projects.blkmgr.virtual;
+
+public class DiskBlock
+{
+    /* public long vIdx; */
+    public final long location ;
+    public final long length ;
+
+    public DiskBlock(long location, long length)
+    {
+        super() ;
+        this.location = location ;
+        this.length = length ;
+    }
+
+    // Valu equality.
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31 ;
+        int result = 1 ;
+        result = prime * result + (int)(length ^ (length >>> 32)) ;
+        result = prime * result + (int)(location ^ (location >>> 32)) ;
+        return result ;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true ;
+        if (obj == null) return false ;
+        if (getClass() != obj.getClass()) return false ;
+        DiskBlock other = (DiskBlock)obj ;
+        if (length != other.length) return false ;
+        if (location != other.location) return false ;
+        return true ;
+    }
+}
---
+++ src/main/java/projects/blkmgr/virtual/VBlockIndexMem.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package projects.blkmgr.virtual;
+
+import java.util.ArrayList ;
+import java.util.List ;
+
+import storage.StorageException ;
+
+public class VBlockIndexMem implements VirtualBlockIndex
+{
+    // See Indirection.
+    
+    // CRUDE
+    private List<DiskBlock> blocks = new ArrayList<>() ;
+    
+    @Override
+    public long alloc(long location, long length)
+    {
+        DiskBlock blk = new DiskBlock(location, length) ;
+        // Scan (!) for nulls.
+        for ( int i = 0 ; i < blocks.size() ; i++ )
+        {
+            if ( blocks.get(i) == null ) 
+            {
+                blocks.set(i, blk) ;
+                return i ;
+            }
+        }
+        blocks.add(blk) ;
+        return blocks.size()-1 ;
+    }
+
+    @Override
+    public DiskBlock get(long vIndex)
+    {
+        check(vIndex) ;
+        return blocks.get((int)vIndex) ;
+    }
+
+    @Override
+    public void set(long vIndex, long location, long length)
+    {
+        DiskBlock blk = new DiskBlock(location, length) ;
+        set(vIndex, blk) ;
+    }
+
+    @Override
+    public void set(long vIndex, DiskBlock pBlock)
+    {
+        check(vIndex) ;
+        blocks.set((int)vIndex, pBlock) ;
+    }
+
+    @Override
+    public void remove(long vIndex)
+    {
+        check(vIndex) ;
+        blocks.set((int)vIndex, null) ;
+    }
+
+    private final void check(long vIndex)
+    {
+        if ( vIndex < 0 || vIndex >= blocks.size() )
+            throw new StorageException("Out of range: "+vIndex+" [0,"+(blocks.size()-1)+"]") ;
+    }
+}
+
