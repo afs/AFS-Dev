fbdf050e37c56e5ec2298727b31e302335aac1e8
Andy Seaborne <andy@seaborne.org>
2015-05-06 14:21:05 +0100

    Dev

--- src/main/java/dev/RunAFS.java
+++ src/main/java/dev/RunAFS.java
@@ -17,57 +17,12 @@
 
 package dev;
 
-import org.apache.jena.atlas.lib.Timer ;
 import org.apache.jena.atlas.logging.LogCtl ;
-import org.apache.jena.riot.RDFDataMgr ;
-import org.apache.jena.sparql.sse.SSE ;
-import org.apache.jena.tdb.base.file.Location ;
-import org.apache.jena.tdb.index.IndexBuilder ;
-import org.apache.jena.tdb.index.RangeIndexBuilder ;
-import org.apache.jena.tdb.setup.* ;
-import org.apache.jena.tdb.store.DatasetGraphTDB ;
 
 public class RunAFS
 {
     static { LogCtl.setCmdLogging(); }
-    
-    static class  DatasetBuilderRadix extends DatasetBuilderStd {
-        DatasetBuilderRadix() { 
-            super() ;
-            setup() ;
-        }
-        
-        private void setup() {
-            StoreParams params = StoreParams.getDftStoreParams() ;
-            ObjectFileBuilder objectFileBuilder = new BuilderStdDB.ObjectFileBuilderStd()  ;
-            
-            IndexBuilder indexBuilder = new IndexBuilderRadix() ;
-            RangeIndexBuilder rangeIndexBuilder = new RangeIndexBuilderRadix() ;
-            
-            NodeTableBuilder nodeTableBuilder = new BuilderStdDB.NodeTableBuilderStd(indexBuilder, objectFileBuilder) ;
-            TupleIndexBuilder tupleIndexBuilder = new BuilderStdDB.TupleIndexBuilderStd(rangeIndexBuilder) ;
-            super.set(nodeTableBuilder, tupleIndexBuilder) ;
-        }
-    }
-    
-
-    public static void main(String ...argv)
-    {
-        LogCtl.setLog4j() ;
-        DatasetBuilder builder = new DatasetBuilderRadix() ;
-        DatasetGraphTDB dsg = builder.build(Location.mem(), StoreParams.getDftStoreParams()) ;
-        
-        //Log.enable(NodeTable.class) ;
-        //dsg.add(SSE.parseQuad("(_ <s> <p> <o>)")) ;
-        //if ( false )
-        Timer t = new Timer() ;
-        t.startTimer() ;
-        RDFDataMgr.read(dsg, "/home/afs/Datasets/BSBM/bsbm-50k.nt.gz") ;
-        long x = t.endTimer() ;
-        System.out.println("load = "+Timer.timeStr(x)+"s") ;
-        
-        if ( false )
-            SSE.write(dsg) ;
+    public static void main(String ...argv) {
     }
 }
 
